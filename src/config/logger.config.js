3770fbf1557b71296e9acbaa918f4b9feafcab02
Subodh Adhikari <subodh1adhikari@gmail.com>
2025-04-04 23:25:00 +0545

    Create base template

---
+++ src/config/logger.config.js
@@ -0,0 +1,19 @@
+import { createLogger, format, transports } from "winston";
+
+const { combine, timestamp, label, printf } = format;
+
+const customFormat = printf(({ level, message, label, timestamp }) => {
+  return `${timestamp} [${label}] ${level}: ${message}`;
+});
+
+export const logger = createLogger({
+  format: combine(
+    label({ label: "airline-reservation-system" }),
+    timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
+    customFormat
+  ),
+  transports: [
+    new transports.Console(),
+    new transports.File({ filename: "combined.log" }),
+  ],
+});
---
+++ src/routes/v1/index.js
@@ -0,0 +1,8 @@
+import express from "express";
+import { info } from "../../controllers/index.js";
+
+const router = express.Router();
+
+router.get("/info", info);
+
+export default router;
